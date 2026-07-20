7b5bb5fc01ce271494868049a8079c50e29567c9
João Victor de Souza Santana <20241ireads0004@ifba.edu.br>
2026-07-20 11:01:11 -0300

    Fix: erros de rota

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // PÁGINAS
 import Home from "./infraestructure/pages/Home";
 import Login from "./infraestructure/pages/Login";
-import AdminPanel from "./infraestructure/pages/AdminPanel";
-import Atividade4 from "./infraestructure/pages/Atividade-4";
 import GrupoEsportivo from "./infraestructure/pages/GrupoEsportivo";
 import EventoEsportivo from "./infraestructure/pages/EventoEsportivo";
 
@@ -24,16 +22,6 @@ function App() {
         <Route
           path="/login"
           element={<Login />}
-        />
-
-        <Route
-          path="/admin"
-          element={<AdminPanel />}
-        />
-        
-        <Route
-          path="/atividade-4"
-          element={<Atividade4 />}
         />
 
         <Route
