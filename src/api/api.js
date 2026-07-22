69f5f05937692a0b48c2c0976ea0f820fbc1ef27
João Victor de Souza Santana <20241ireads0004@ifba.edu.br>
2026-07-22 13:17:12 -0300

    Add: Rota do backend no Api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,17 @@
-import axios from "axios";
+import axios from 'axios';
 
-const api = axios.create({
-    baseURL: "http://localhost:8080",
-    headers: {
-        "Content-Type": "application/json"
-    }
+// Define a URL base para todas as chamadas da API
+const API_BASE_URL = 'https://prg04joaovictor-backend.onrender.com'; 
+
+export const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-export default api;
+// Exemplo de chamada aos eventos esportivos
+export const getEventos = async () => {
+  const response = await api.get('/eventosesportivos');
+  return response.data;
+};
